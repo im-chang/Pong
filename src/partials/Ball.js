0a5e4cf874e47a6a3bcf787be1576900015a108a
im-chang <mchang0718@gmail.com>
2018-05-11 09:46:51 -0700

    first movement of ball

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -6,14 +6,24 @@ export default class Ball {
       this.boardWidth = boardWidth;
       this.boardHeight = boardHeight;
       this.direction = 1;
+      this.reset();
+    }
+
+    reset() {
+      this.x = this.boardWidth / 2;
+      this.y = this.boardHeight / 2;
+      this.vy = Math.round(Math.random() * 10 - 5);
+      this.vx = this.direction * (6 - Math.abs(this.vy));
     }
 
     render(svg, player1, player2) {
-        //...
+      this.x += this.vx;
+      this.y += this.vy;
+      
       let circle = document.createElementNS(SVG_NS, 'circle');
       circle.setAttributeNS(null, 'fill', 'orange');
-      circle.setAttributeNS(null, 'cx', this.boardWidth / 2);
-      circle.setAttributeNS(null, 'cy', this.boardHeight / 2);
+      circle.setAttributeNS(null, 'cx', this.x);
+      circle.setAttributeNS(null, 'cy', this.y);
       circle.setAttributeNS(null, 'r', this.radius);
 
 
